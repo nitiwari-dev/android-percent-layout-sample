e688d69e1d8817ddd5b6fed13a5823b84f2c02b1
nitiwari-dev <nitiwari.dev@gmail.com>
2016-09-28 01:05:44 +0530

    [configuration]

---
+++ app/src/main/java/com/code2concept/percent_layout_sample/adapter/PercentAdapter.java
@@ -0,0 +1,93 @@
+package com.code2concept.percent_layout_sample.adapter;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.code2concept.percent_layout_sample.R;
+import com.code2concept.percent_layout_sample.holder.PercentHolder;
+import com.code2concept.percent_layout_sample.model.PercentModel;
+
+import java.util.List;
+
+/**
+ * Created by nitesh on 8/30/2015.
+ */
+public class PercentAdapter extends RecyclerView.Adapter<PercentHolder> {
+
+    private final List<PercentModel> percentModelList;
+    private Context context;
+
+    public PercentAdapter(Context context, List<PercentModel> percentModelList){
+        this.percentModelList = percentModelList;
+        this.context = context;
+    }
+    @Override
+    public PercentHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View itemView = LayoutInflater.
+                from(context).
+                inflate(R.layout.row_percent_cardview, parent, false);
+
+
+        return new PercentHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(PercentHolder holder, final int position) {
+        holder.percentTv.setText(percentModelList.get(position).percentType);
+        holder.percentTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPercentDialog(position, percentModelList.get(position).percentType);
+            }
+        });
+    }
+
+
+    private void showPercentDialog(int position, String percentType) {
+
+        switch (position){
+
+
+            case 0:
+                createDialogByPosition(percentType , R.layout.row_percent_20_50);
+                break;
+
+            case 1:
+                createDialogByPosition(percentType , R.layout.row_percent_50_50);
+                break;
+
+            case 2:
+                createDialogByPosition(percentType , R.layout.row_percent_50_100);
+                break;
+
+            case 3:
+                createDialogByPosition(percentType , R.layout.row_percent_tile1);
+                break;
+
+            case 4:
+                createDialogByPosition(percentType , R.layout.row_percent_tile2);
+                break;
+        }
+
+
+
+    }
+
+    private void createDialogByPosition(String percentType, int resID) {
+
+        final Dialog dialog = new Dialog(context);
+        dialog.setContentView(resID);
+        dialog.setTitle(percentType);
+        dialog.show();    }
+
+
+    @Override
+    public int getItemCount() {
+        return percentModelList.size();
+    }
+}
